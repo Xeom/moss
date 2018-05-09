9881d380fabe57a8b136e2a34a5d41795f1f9a51
Xeom <fwharf.fw@gmail.com>
2018-05-09 17:20:42 +0100

    Updated test to test tasks

--- test.c
+++ test.c
@@ -1,34 +1,34 @@
 #include "mem.h"
 #include "pipe.h"
 #include "com.h"
+#include "task.h"
 #include <util/delay.h>
-#include <avr/interrupt.h>
-int8_t os_com_transmit(os_pipe *p);
-typedef struct os_com_s os_com;
-struct os_com_s
+
+void print_hellos(void)
 {
-    volatile uint8_t *data_reg;
-    volatile uint8_t *ready_reg;
-    uint8_t  ready_bit : 3;
-    uint8_t  ready     : 1;
-    uint8_t  timeout;
-    uint16_t baud;
-};
+    for (;;)
+    {
+        os_write_str(&uart0_tx, "Hello I am also happy\r\n");
+        os_yield();
+    }
+}
+
+void print_goodbyes(void)
+{
+    for (;;)
+    {
+        os_write_str(&uart0_tx, "Goodbye, but I am happy\r\n");
+        os_yield();
+    }
+}
 
 int main(void)
 {
-    char *ptr;
     os_mem_init();
     os_com_init();
-    sei();
-    //for(;;)    os_mem_print(&usart0);
-    os_alloc(100);
-    ptr = os_alloc(100);
-    os_alloc(100);
-    os_free(ptr);
-    os_alloc(50);
- os_alloc(10);
-    for(;;){
-    os_mem_print(&uart0_tx);
-    _delay_ms(1000);}
+
+    os_new_task(print_hellos, 500);
+    print_goodbyes();
+
+    return 0;
 }
