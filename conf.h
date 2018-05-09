83c6ff79d409d06667b93d231cb07241bd1e5813
Xeom <fwharf.fw@gmail.com>
2018-05-09 23:30:17 +0100

    New test

--- conf.h
+++ conf.h
@@ -1,12 +1,21 @@
 #if !defined(OS_CONF_H)
 # define OS_CONF_H
 
-# define OS_DBG
-# define OS_MEM_CHKSUM
+//# define OS_DBG
+//# define OS_MEM_CHKSUM
 # define OS_COM
 
 # define OS_PIPE_SIZE 32
 # define OS_NUM_TASKS 32
+
+# define OS_UART_ALL
+
+# if defined(OS_UART_ALL)
+#  define OS_UART_TX0
+#  define OS_UART_TX1
+#  define OS_UART_RX0
+#  define OS_UART_RX1
+# endif /* OS_UART_ALL */
 
 # if defined(OS_DBG)
 #  define OS_MEM_DBG
--- test.c
+++ test.c
@@ -4,31 +4,38 @@
 #include "task.h"
 #include <util/delay.h>
 
-void print_hellos(void)
+
+void print_hellos(void *p)
 {
     for (;;)
     {
-        os_write_str(&uart0_tx, "Hello I am also happy\r\n");
+//        os_mem_print(&uart0_tx);
         os_yield();
     }
 }
 
-void print_goodbyes(void)
+void print_str(void *str)
 {
     for (;;)
     {
-        os_write_str(&uart0_tx, "Goodbye, but I am happy\r\n");
+        _delay_ms(50);
+        os_write_str(&uart0_tx, str);
         os_yield();
     }
 }
+
+char *wow = "Wowzers\r\n";
+char *woah = "Woah\r\n";
 
 int main(void)
 {
     os_mem_init();
     os_com_init();
 
-    os_new_task(print_hellos, 500);
-    print_goodbyes();
+    os_new_task(print_str, 500, wow);
+    os_new_task(print_str, 500, woah);
+
+    os_kill_task(os_task_curr);
 
     return 0;
 }
